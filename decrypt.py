80845a0045f1c3bb68874b4ede6c6be4fe6937d6
Berkeley Clayton <berkeley.clayton@donvale.vic.edu.au>
2025-09-10 22:06:47 +1000

    Implemented ASCII character combatibility (instead of just numbers)

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,15 @@
+def convert_number_to_string(number):
+    n = number
+    b = []
+    string = ''
+    while n > 0:
+        b.append(n % 256)
+        n = n // 256
+    b.reverse()
+    for i, byte in enumerate(b):
+        b[i] = chr(byte)
+    return ''.join(b)
+
 with open("rsakey.private", 'r') as file:
     data = file.read().split(", ")
     n, d = int(data[0]), int(data[1])
@@ -9,4 +21,4 @@ with open(fileToDecrypt, 'r') as file:
     message = pow(c, d, n)
 
 with open(f"{fileToDecrypt}.decrypted", 'w') as file:
-    file.writelines(str(message))
+    file.writelines(convert_number_to_string(message))
--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,13 @@
+def convert_string_to_numbers(string):
+    b = []
+    num = 0
+    for char in string:
+        b.append(int(format(ord(char), '08b'), 2))
+    for i, byte in enumerate(b):
+        num += byte * (256 ** (len(b) - (i + 1)))
+    return num
+
+
 with open("rsakey.public", 'r') as file:
     data = file.read().split(", ")
     n, e = int(data[0]), int(data[1])
@@ -5,7 +15,7 @@ with open("rsakey.public", 'r') as file:
 fileToEncrypt = input("File to encrypt: ")
 
 with open(fileToEncrypt, 'r') as file:
-    data = int(file.read())
+    data = convert_string_to_numbers(file.read())
     c = pow(data, e, n)
 
 with open(f"{fileToEncrypt}.encrypted", 'w') as file:
